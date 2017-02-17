518118f7436807adf6f9e8d4c6a6e84752110885
Jim Padilla <jp_code2013@yahoo.com>
2017-02-17 10:35:08 -0800

    Add new/create controller for product, add form and flash for new product

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,32 @@
+class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index]
+  before_action :set_product, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash.now[:success] = "Product Successfully Saved"
+      redirect_to product_path(@product)
+    else
+      flash.now[:warning] = "Product Not Saved"
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :image)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+end
