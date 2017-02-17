43288c7add036db5d9dba52222c7e49fb2369bb9
Jim Padilla <jp_code2013@yahoo.com>
2017-02-17 11:47:56 -0800

    Add edit/delete functionality to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,9 +10,36 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to product_path(@product)
     else
-      flash[:warning] = 'Review not saved'
+      flash[:warning] = 'Review Not Saved'
       render :new
     end
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    @user = User.find(@review.user_id)
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    @user = User.find(@review.user_id)
+    if @review.update(review_params)
+      flash[:success] = 'Review Successfully Updated'
+      redirect_to product_path(@product)
+    else
+      flash[:warning] = 'Review Not Updated'
+      redirect_to product_path(@product)
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:success] = 'Successfully Deleted Review'
+    redirect_to product_path(@product)
   end
 
   private
