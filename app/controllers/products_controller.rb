3557148548eb4be096e9e6fa810689aecab49bc9
Jim Padilla <jp_code2013@yahoo.com>
2017-02-17 10:43:39 -0800

    Create flash messages partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
   private
   def product_params
     params.require(:product).permit(:title, :description, :price, :image)
